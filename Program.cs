aca516cdfee940586f31d4841b977ea5e6b17f78
sabillarhyn <salsabilla.rohyani@students.amikom.ac.id>
2020-04-14 10:44:55 +0700

    Responsi Pemrograman

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ResponsiPemrograman2690
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan karyawan = new karyawan();
+            karyawan karyawan2 = new karyawan();
+
+            karyawan.No = "1.";
+            karyawan.Nama = "Paijo";
+            karyawan.Nik = 190302123;
+            karyawan.GajiBulanan = 3000000;
+
+            karyawan2.No = "2.";
+            karyawan2.Nama = "Jono";
+            karyawan2.Nik = 190302124;
+            karyawan2.GajiBulanan = 2000000;
+
+            Console.WriteLine("No      NIK/Nama	     	Gaji Bulanan");
+            Console.WriteLine("-------------------------------------------------");
+            karyawan.DataGaji();
+            karyawan2.DataGaji();
+            
+            Console.WriteLine("Kenaikan Gaji 10%!!");
+            Console.WriteLine("No      NIK/Nama	     	Gaji Bulanan");
+            Console.WriteLine("-------------------------------------------------");
+            karyawan.NaikGaji();
+            karyawan2.NaikGaji();
+        }
+    }
+}
---
+++ karyawan.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResponsiPemrograman2690
+{
+	class karyawan
+	{
+		public string No { get; set; }
+		public string Nama { get; set; }
+		public int Nik { get; set; }
+		public int GajiBulanan { get; set; }
+
+		public void DataGaji()
+		{
+			Console.WriteLine("{0}   {1} {2}		{3}", No, Nik, Nama, GajiBulanan);
+		}
+		
+		public void NaikGaji()
+		{
+			Console.WriteLine("{0}   {1} {2}		{3}", No, Nik, Nama, GajiBulanan+(GajiBulanan*0.1));
+		}
+	}
+}
